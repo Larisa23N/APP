315f314e4d1a89db34626e33d34f0012137513b1
Larisa23N <142898628+Larisa23N@users.noreply.github.com>
2023-10-12 07:50:59 +0200

    edited js

--- src/index.mjs
+++ src/index.mjs
@@ -9,27 +9,47 @@ let months = ["January","February","March","April","May","June","July","August",
 let currentMonth = months[now.getMonth()];
 
 let h3 = document.querySelector("h3")
-h3.innerHTML = `${currentDay} ${currentMonth} ${currentDate} ${currentMonth}, ${currentHours}:${currentMinutes} `;
+h3.innerHTML = `${currentDay} ${currentMonth} ${currentDate}, ${currentHours}:${currentMinutes} `;
+
 function showWeather(response){
   console.log(response.data)
-let h4 = document.querySelector("h4")
-let temperature = Math.round(response.data.main.temp)
-h4.innerHTML = `${temperature}°C`
+let cityElement= document.querySelector("#city");
+let windElement=document.querySelector("#speed");
+let humidityElement=document.querySelector("#humid");
 let h1 = document.querySelector("h1");
-  h1.innerHTML = response.data.name;
-}
-function search(event){
-  event.preventDefault()
-  let searchInput= document.querySelector("#search-text-input");
-  let h1 =document.querySelector("h1");
-  let api ="866a208a73eeff02182218e9441647a1";
-  let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric`;
-axios.get(`${apiUrl}&appid=${api}`).then(showWeather)
-h1.innerHTML = searchInput.value
+let iconElement = document.querySelector("#icon")
+let temperatureElement= document.querySelector("#temp")
+
+
+
+temperatureElement= Math.round(response.data.main.temp)
+h1.innerHTML = response.data.name;
+windElement.innerHTML= response.data.wind.speed;
+humidityElement.innerHTML= response.data.main.humidity
+cityElement= response.data.name;
+iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
+function search(city){
+let api ="866a208a73eeff02182218e9441647a1";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+axios.get(`${apiUrl}&appid=${api}`).then(showWeather)
+}
+
+
+function handleSubmit(event){
+  event.preventDefault();
+  let searchInput= document.querySelector("#search-text-input");
+  search(searchInput.value)
+  console.log(searchInput)
+}
+search("Sandton")
 let form = document.querySelector("#search-form")
-form.addEventListener("submit",search)
+form.addEventListener("submit",handleSubmit)
 
 //function convertToF(event) {
 //  let currentTemp = document.querySelector("#current-temperature");
